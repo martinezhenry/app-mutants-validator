f30bd83cf55437409235d6ffc5660db73e397b5c
Henry Martinez <7996045+martinezhenry@users.noreply.github.com>
2021-06-07 15:58:44 -0500

    update run script, inclu basedir

--- scripts/run-app.sh
+++ scripts/run-app.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+## getting script dir
+BASEDIR=$(dirname "$0")
+
+## moving to BASEDIR or exit
+cd "$BASEDIR" || exit
+
+## moving to root app dir
+cd ..
+
 ## Running with docker-compose
 docker-compose up --force-recreate --build  -d
