9efd4d71fb13b28a42e8a500f96f75a6a8b724bc
Henry Martinez <7996045+martinezhenry@users.noreply.github.com>
2021-06-07 22:44:30 -0500

    add docker with image file

---
+++ scripts/run-with-image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## getting script dir
+BASEDIR=$(dirname "$0")
+
+## moving to BASEDIR or exit
+cd "$BASEDIR" || exit
+
+docker pull "$IMAGE"
+
+docker-compose -f ./deploy/docker-compose.yml up --remove-orphans --force-recreate --build -d
+
+
