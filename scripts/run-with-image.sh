40978cba227ced7c5a2f07a3b999bbea621ead55
Henry Martinez <7996045+martinezhenry@users.noreply.github.com>
2021-06-07 22:52:39 -0500

    add docker with image file

--- scripts/run-with-image.sh
+++ scripts/run-with-image.sh
@@ -8,6 +8,6 @@ cd "$BASEDIR" || exit
 
 docker pull "$IMAGE"
 
-docker-compose -f ./deploy/docker-compose.yml up --remove-orphans --force-recreate --build -d
+docker-compose -f ./deploy/docker-compose.yml up --remove-orphans --force-recreate --build -d -p mutant
 
 
